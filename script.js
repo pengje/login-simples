130ef6e0be156a422aad85149938692878fcba22
pengje <zaboskipedro@gmail.com>
2024-02-07 01:50:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+    const botao = document.getElementById('clickme');
+
+    botao.addEventListener('click', function() {
+    // Aqui você pode definir o URL para o qual deseja redirecionar o usuário
+    const url = "https://github.com/pengje";
+    window.open(url, '_blank');
+});
